1d7f40e379ba4060fa1956e25910fd318f891b81
JXLOVE2020\jxlov <jxlove2020@gmail.com>
2021-01-13 06:11:30 +0900

    first Webpack Setting

---
+++ 2021/react/firstWebpack/client.jsx
@@ -0,0 +1,14 @@
+const React = Require('react');
+const ReactDom = Require('react-dom');
+
+class WordRelay extends React.Component {
+    state = {
+
+    };
+
+    render() {
+
+    }
+}
+
+ReactDom.render(<WordRelay />, document.querySelector('#root'));
