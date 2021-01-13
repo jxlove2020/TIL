9d31d35bcb6eff6998cfa2b12c5b78c879437864
JXLOVE2020\jxlov <jxlove2020@gmail.com>
2021-01-13 10:12:12 +0900

    웹 팩 설정 부분 추가

--- 2021/react/gugudanWebpack/webpack.config.js
+++ 2021/react/gugudanWebpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development', //production
@@ -15,11 +16,23 @@ module.exports = {
             test: /\.jsx?$/, 
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            // https://github.com/browserslist/browserslist
+                            browsers: ['> 5% in KR', 'last 2 chrome versions'], 
+                        },
+                        debug: true,
+                    }], 
+                    '@babel/preset-react',
+                ],
                 plugins: [],
             },
         }],
     },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }), // debug: true 세팅 해주는 플러그인
+    ],
     output: {
         filename: 'app.js',
         path: path.join(__dirname, 'dist'),
