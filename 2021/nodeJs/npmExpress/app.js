993a7ff536f77a23dbbba605fb5f39ccba61f79c
JXLOVE2020\jxlov <jxlove2020@gmail.com>
2021-01-18 07:29:27 +0900

    npmExpress study

--- 2021/nodeJs/npmExpress/app.js
+++ 2021/nodeJs/npmExpress/app.js
@@ -1,7 +1,42 @@
+// 요청 주소의 순서가 중요함. 순서대로 실행하기 때문에 순서가 중요함
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const multer = require('multer');
 
 const app = express();
+
+//app.set('port', process.env.PORT || 3000);
+
+// 클라이언트에서 요청했을 때 어떤 응답을 했는지 서버에 기록
+app.use(morgan('dev')); // dev (개발시), combined (배포시)
+
+// 요청 경로와 실제경로가 달라서 보안에 도움이 됩니다.
+// app.use('요청경로', express.static('실제경로'));
+app.use('/', express.static(__dirname, 'public16')); // public 은 알려져 있어서 다른 이름 사용
+
+app.use(cookieParser(process.env.COOKIE_SECRET)); // signedCookies
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET, // 서명되어 읽을 수 없는 글자로 표시
+    cookie: {
+        httpOnly: true, // javascript 공격 방지
+        secure: false,
+    },
+    name: 'sessionCookie' // 기본적으로 connect.sid 로 되어 있음
+}))
+
+// express-session :  body-parser 대신 사용
+app.use(express.json()); // 클라이언트에서 json 데이터 보낼 때 데어터 파싱해 줌
+// 폼 서브밋 할 때 : 폼 파싱 
+app.use(express.urlencoded({ extended: true })); // extended true 면 qs 모드 사용 false 면 querystring 사용
 
 // next 인자로 다음 router 실행
 app.use((req, res, next) => {
@@ -10,6 +45,27 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
+
+    // // express-session // 개인의 저장 공간
+    // req.session.id = 'hello'
+
+    // // 쿠키가 있으면 알아서 넣어줌 
+    // req.cookies; // {mycookie: test}
+    // // 서명된 쿠키
+    // req.signedCookies;
+
+    // // 'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`
+    // res.cookie('name', encodeURIComponent(name), {
+    //     expires: new Date(),
+    //     httpOnly: true,
+    //     path: '/'
+    // });
+    // // 쿠키 지울 때 ( 옵션이 같아 야 함 )
+    // res.clearCookie('name', encodeURIComponent(name), {
+    //     httpOnly: true,
+    //     path: '/'
+    // });
+
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
--- 2021/nodeJs/npmExpress/readme.txt
+++ 2021/nodeJs/npmExpress/readme.txt
@@ -2,3 +2,26 @@
 npm i -D nodemon 설치시 설치가 되지 않으면
 i 대신 install 로 설치
 -D 옵션은 제일 뒤에 붙여줘도 된다.
+
+// morgan 클라이언트에서 어떤요청이 왔는지 콘솔에 찍어준다.
+// cookie-parser 쿠키 관련
+// express-session 세션 관련 ( 이전에는 body-parser 사용 )
+npm i morgan cookie-parser express-session
+
+// dotenv : 소스코드의 키를 관리, 환경변수에 비밀키를 넣어두는것 
+// .env 파일을 생성하여 그곳에서 관리 
+// 깃 에서는 ignore 하여 올리지 말것 // 관리 철저하게 해야 함
+npm i dotenv
+
+// 템플릿엔진은 둘 중 하나 선택 사용 pug , nunjucks
+// pug 템플릿엔진을 사용하려면 아래 내용 추가
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// nunjucks 템플릿엔진을 사용하려면 아래 내용 추가
+const nunjucks = require('nunjucks');
+app.set('view engine', 'html'); // html, njk 중 하나 사용
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
