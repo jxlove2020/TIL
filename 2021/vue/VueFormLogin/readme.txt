dc1be554ca57df2c23520717cc03dbf0f71f8363
HyeokRae Jo <jxlove2020@gmail.com>
2021-01-26 23:58:00 +0900

    vue study

---
+++ 2021/vue/VueFormLogin/readme.txt
@@ -0,0 +1,18 @@
+// VueCLI 를 활용하여 프로젝트 생성
+1. vue create vue-form
+2. cd vue-form
+3. npm run serve
+
+components 에서 HelloWorld.vue 삭제
+App.vue 에서 다 지우고 vu 탭 해서 템플릿 생성
+template 에서 div 하나 생성 root
+
+components 에 파일명 두 단어로 조합
+브라우저의 태그인지 컴포넌트 태그인지 구분하기 위해서 두단어로 조합함
+
+AppHeader.vue 생성
+vu 탭 해서 템플릿 생성
+template 안에 header 내용 추가
+<header>
+    <h1>Header</h1>
+</header>
---
+++ 2021/vue/VueFormLogin/vue-form/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id :  </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw :  </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  // 객체를 많이 사용했었지만 
+  // 값이 참조가 되서 컴포넌트간의 데이터가 공유되지 않도록 
+  // 새 개체를 리턴해주는 구문 사용 
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      
+      var url = 'https://jsonplaceholder.typicode.com/users'
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+
+      axios.post(url, data)
+      .then(function(response){
+        console.log(response);
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
