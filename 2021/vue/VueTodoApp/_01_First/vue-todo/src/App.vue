d4bce294096b67095d4f2b868d8f1cdd243f2c61
HyeokRae Jo <jxlove2020@gmail.com>
2021-01-28 00:30:28 +0900

    컴포넌트 등록과 인덱스 페이지 생성

---
+++ 2021/vue/VueTodoApp/_01_First/vue-todo/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+// var my_component = {
+//   template: '<div>my component</div>'
+// };
+
+// new Vue({
+//   el: '',
+//   components: {
+//     'my-component': my_component
+//   }
+// });
+
+export default {
+  name: 'App',
+  components: {
+    // '컴포넌트 태그명': 컴포넌트 내용
+    'TodoHeader':TodoHeader,
+    'TodoInput':TodoInput,
+    'TodoList':TodoList,
+    'TodoFooter':TodoFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
