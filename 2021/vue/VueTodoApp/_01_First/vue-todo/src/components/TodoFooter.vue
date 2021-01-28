00178da250fcc0d17499fd5021d8b8b694dd1821
HyeokRae Jo <jxlove2020@gmail.com>
2021-01-29 01:52:45 +0900

    전체 삭제 버튼 구현

--- 2021/vue/VueTodoApp/_01_First/vue-todo/src/components/TodoFooter.vue
+++ 2021/vue/VueTodoApp/_01_First/vue-todo/src/components/TodoFooter.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>footer</div>
+  <div>
+    <!-- clearAll 전체 삭제 버튼 -->
+    <div class="clearAllContainer">
+      <span class="clearAllBtn" v-on:click="clearTodo">ClearAll</span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    clearTodo: function() {
+      // 로컬스토리지 데이터 전체 지우기
+      localStorage.clear();
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+  cursor: pointer;
+}
 </style>
