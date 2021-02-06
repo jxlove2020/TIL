8a7c51c5fe9586b753f23ec4b951c29ac1fa67cc
HyeokRae Jo <jxlove2020@gmail.com>
2021-02-07 02:26:24 +0900

    computed 로 store state 접근방법1

--- 2021/vue/VueHackerNewsApp/hacker-news/src/views/AskView.vue
+++ 2021/vue/VueHackerNewsApp/hacker-news/src/views/AskView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="(item,i) in this.$store.state.ask" :key=i>{{ item.title }}</div> 
+    <div v-for="(item,i) in ask" :key=i>{{ item.title }}</div> 
   </div>
 </template>
 
@@ -13,6 +13,11 @@ export default {
   //     ask: []
   //   }
   // },
+  computed: {
+    ask() {
+      return this.$store.state.ask
+    }
+  },
   created() {
     this.$store.dispatch('FETCH_ASK')
     // // var vm = this; // this 를 그대로 가져가기 위해 화살표 함수를 사용
