a784140ec5a3f6925f766eec370f47623f7c9042
HyeokRae Jo <jxlove2020@gmail.com>
2021-02-08 05:36:27 +0900

    유저프로필 컴포넌트 1

---
+++ 2021/vue/VueHackerNewsApp/hacker-news/src/components/UserProfile.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <!-- <router-link :to="`/user/${userInfo.user}`">
+            {{ userInfo.id }}
+          </router-link> -->
+          <div>{{ userInfo.id }}</div>
+          <div class="time">
+            {{ userInfo.created }}
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        userInfo() {
+            return this.$store.state.user;
+        }
+    }
+}
+</script>
+
+<style scoped>
+  .user-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+  }
+  .fa-user {
+    font-size: 2.5rem;
+  }
+  .user-description {
+    padding-left: 0.5rem;
+  }
+  .time {
+    font-size: 0.7rem;
+  }
+</style>
--- 2021/vue/VueHackerNewsApp/hacker-news/src/views/UserView.vue
+++ 2021/vue/VueHackerNewsApp/hacker-news/src/views/UserView.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
-    <p>name : {{userInfo.id}}</p>
+    <!-- <p>name : {{userInfo.id}}</p>
     <p>karma : {{userInfo.karma}}</p>
-    <p>created : {{userInfo.created}}</p>
+    <p>created : {{userInfo.created}}</p> -->
+    <user-profile></user-profile>
   </div>
 </template>
 
 <script>
-
+import UserProfile from '../components/UserProfile.vue'
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
-    userInfo() {
-      return this.$store.state.user;
-    }
+    // userInfo() {
+    //   return this.$store.state.user;
+    // }
   },
   created() {
     // console.log(this.$route.params.id);
