998769beac151f540af06081ad8e03bd42fa73e6
HyeokRae Jo <jxlove2020@gmail.com>
2021-02-14 06:00:59 +0900

    컴포넌트 통신을 이용한 차트 컴포넌트 기능 결합

--- 2021/vue/VueCharts/chart-lib/src/App.vue
+++ 2021/vue/VueCharts/chart-lib/src/App.vue
@@ -3,7 +3,7 @@
     <h1>Chart.js</h1>
     
     <h3>BarChart</h3>
-    <bar-chart></bar-chart>
+    <bar-chart v-bind:propsdata="chartDataSet"></bar-chart>
 
     <h3>LineChart</h3>
     <line-chart></line-chart>
@@ -19,6 +19,39 @@ export default {
       BarChart,
       LineChart,
   },
+  data() {
+    return {
+      // created 에서 데이터 불러온 값이 아래와 같이 저장되있다고 가정
+      chartDataSet: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }],
+    }
+  },
+  created() {
+    // 데이터 불러오기
+    // data = axios.get('')
+    //       .then(response => this.chartDataSet = response.data)
+    //       .catch(error => console.log(error))
+
+  }
 }
 </script>
 
--- 2021/vue/VueCharts/chart-lib/src/components/BarChart.vue
+++ 2021/vue/VueCharts/chart-lib/src/components/BarChart.vue
@@ -7,6 +7,7 @@
 // 차트 플러그인 사용으로 인해 주석 처리
 
 export default {
+    props: ['propsdata'],
     // 컴포넌트 속성 && 인스턴스 속성
     mounted() {
         // let ctx = document.getElementById('barChart');
@@ -18,27 +19,7 @@ export default {
             type: 'bar',
             data: {
                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+                datasets: this.propsdata // 데이터 연결
             },
             options: {
                 scales: {
