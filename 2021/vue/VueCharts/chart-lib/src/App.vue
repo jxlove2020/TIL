d3c9ecdd1d8a71238966de98d27bb31ac6614988
HyeokRae Jo <jxlove2020@gmail.com>
2021-02-14 05:23:50 +0900

    차트 컴포넌트화, ref 속성

--- 2021/vue/VueCharts/chart-lib/src/App.vue
+++ 2021/vue/VueCharts/chart-lib/src/App.vue
@@ -1,54 +1,24 @@
 <template>
   <div>
     <h1>Chart.js</h1>
-    <canvas id="myChart" width="400" height="400"></canvas>
+    
+    <h3>BarChart</h3>
+    <bar-chart></bar-chart>
+
+    <h3>LineChart</h3>
+    <line-chart></line-chart>
   </div>
 </template>
 
 <script>
-import Chart from 'chart.js';
+import BarChart from './components/BarChart.vue';
+import LineChart from './components/LineChart.vue';
 
 export default {
-  // 컴포넌트 속성 && 인스턴스 속성
-  mounted() {
-    let ctx = document.getElementById('myChart');
-    let myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
-  }
+  components: {
+      BarChart,
+      LineChart,
+  },
 }
 </script>
 
---
+++ 2021/vue/VueCharts/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <canvas ref="lineChart" id="lineChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js'
+
+export default {
+        // 컴포넌트 속성 && 인스턴스 속성
+    mounted() {
+        // var ctx = document.getElementById('lineChart').getContext('2d');
+        var ctx = this.$refs.lineChart.getContext('2d');
+        
+        // var lineChart = new Chart(ctx, {
+        new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45]
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
