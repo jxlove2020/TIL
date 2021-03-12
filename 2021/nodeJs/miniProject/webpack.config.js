510a5e03087e01558665ca0b1be192e74586096f
HyeokRae Jo <jxlove2020@gmail.com>
2021-03-12 13:01:27 +0900

    HtmlWebpackPlugin 추가

--- 2021/nodeJs/miniProject/webpack.config.js
+++ 2021/nodeJs/miniProject/webpack.config.js
@@ -1,4 +1,5 @@
 const  path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     // "production" | "development" | "none"
@@ -29,6 +30,11 @@ module.exports = {
                 }
             }         
         }]
-    }
-
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // index.html 템플릿을 기반으로 빌드 결과물을 추가해 줌
+            template: 'index.html',
+        }),
+    ],
 }
