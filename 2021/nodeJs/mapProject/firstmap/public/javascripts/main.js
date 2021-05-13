7f27d191e9656d22354041a39e8ca4ee9bd08b7b
Joseph <jxlove2020@gmail.com>
2021-05-13 15:44:59 +0900

    투명한 인포윈도우 창 작성

--- 2021/nodeJs/mapProject/firstmap/public/javascripts/main.js
+++ 2021/nodeJs/mapProject/firstmap/public/javascripts/main.js
@@ -20,6 +20,23 @@ const data = [
     }
 ];
 
+let markerList = [];
+let infowindowList = [];
+
+// 다시 한번 함수를 호출하는 이유는 
+// for 문이 돌면서 함수를 실행해버리면 안되기 때문에 
+// 마커가 클릭되었을 때 실행되게 하려고 
+const getClickhandler = (i) => () => {
+    const marker = markerList[i];
+    const infowindow = infowindowList[i];
+
+    if ( infowindow.getMap()) {
+        infowindow.close();
+    } else {
+        infowindow.open(map, marker);
+    }
+}
+
 // data 에 있는 정보를 하나씩 마커표시
 for (let i in data) {
     const target = data[i];
@@ -30,7 +47,28 @@ for (let i in data) {
         position: latlng,
         // 마커생성
         icon: {
-            content: `<div class='marker'></div>`
-        }
-    })
+            content: `<div class='marker'></div>`,
+            anchor: new naver.maps.Point(7.5, 7.5), // 마커의 1/2 크기로 지정
+        },
+    });
+
+    const content = `
+        <div class="infowindow_wrap">
+            <div class="infowindow_title">${target.title}</div>
+            <div class="infowindow_title">${target.address}</div>
+        </div>
+    `;
+    const infowindow = new naver.maps.InfoWindow({
+        content: content,
+        backgroundColor: "#00ff0000",
+        borderColor: "#00ff0000",
+        anchorSize: new naver.maps.Size(0,0),  // 말풍선 꼬리표 제거
+    });
+
+    markerList.push(marker);
+    infowindowList.push(infowindow);    
+}
+
+for(let i = 0, ii = markerList.length; i < ii; i++) {
+    naver.maps.Event.addListener(markerList[i], "click", getClickhandler(i));
 }
