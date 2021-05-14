19804626975568374db0853626091a233b612aa5
Joseph <jxlove2020@gmail.com>
2021-05-14 15:13:07 +0900

    몽고디비 저장 API

--- 2021/nodeJs/mapProject/firstmap/routes/index.js
+++ 2021/nodeJs/mapProject/firstmap/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const locationModel = require('../model/location')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -8,6 +9,28 @@ router.get('/', (req, res, next) => {
 
 router.get('/upload', (req, res, next) => {
   res.render('upload', { title: 'Express' });
+});
+
+// 비구조화 할당 post 방식의 API - 데이터베이스 저장
+router.post('/location', (req, res, next) => {
+  const { title, address, lat, lng } = req.body;
+  let location = new locationModel();
+  location.title = title;
+  location.address = address;
+  location.lat = lat;
+  location.lng = lng;
+  location.save().then((result) => {
+    console.log(result);
+    res.json({
+      message: "success"
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    res.json({
+      message: "error",
+    });
+  });
 });
 
 // 테스트 API - 포스트 맨에서 확인 ===================================
