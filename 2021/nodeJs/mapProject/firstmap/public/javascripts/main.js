2987c74df1c0e746dc16a5a3034c89451a762429
Joseph <jxlove2020@gmail.com>
2021-05-17 09:56:28 +0900

    행정구역 표시

--- 2021/nodeJs/mapProject/firstmap/public/javascripts/main.js
+++ 2021/nodeJs/mapProject/firstmap/public/javascripts/main.js
@@ -97,4 +97,91 @@ $.ajax({
             $(clusterMarker.getElement()).find("div:first-child").text(count);
         },
     });
-})
+});
+
+// 행정구역 지도 나누기 (네이버에서 제공 되는 행정구역 데이터는 예시용이기 때문에 공공데이터 api 로 교체 해야 함)
+const urlPrefix = "https://navermaps.github.io/maps.js/docs/data/region";
+const urlSuffix = ".json"
+
+let regionGeoJson = [];
+let loadCount = 0;
+
+const tooltip = $(
+    `<div style="position:absolute; z-index:1000; padding: 5px 10px; background: white; border: 1px solid black; font-size: 14px; display: none; ponter-events: none"></div>`
+);
+
+tooltip.appendTo(map.getPanes().floatPane);
+
+naver.maps.Event.once(map, "init_stylemap", () => {
+    for(let i=1; i<18; i++){
+        let keyword = i.toString();
+        if(keyword.length === 1) {
+            keyword = "0" + keyword;
+        }
+        $.ajax({
+            url: urlPrefix + keyword + urlSuffix,
+        }).done((geojson) =>{
+            regionGeoJson.push(geojson);
+            loadCount++;
+            if(loadCount === 17) {
+                startDataLayer();
+            }
+        });
+    }
+});
+
+function startDataLayer() {
+    map.data.setStyle((feature) => {
+        const styleOptions = {
+            fillColor: "#ff0000",
+            fillOpacity: 0.0001,
+            strokeColor: "#ff0000",
+            strokeWeight: 2,
+            strokeOpacity: 0.4,
+        };
+
+        if(feature.getProperty("focus")) {
+            styleOptions.fillOpacity = 0.6;
+            styleOptions.fillColor = "#0f0";
+            styleOptions.strokeColor = "#0f0";
+            styleOptions.strokeWeight = 4;
+            styleOptions.strokeOpacity = 1;
+        }
+
+        return styleOptions;
+    });
+
+    // 각 행정구역 표시
+    regionGeoJson.forEach((geojson) => {
+        map.data.addGeoJson(geojson);
+    });
+
+    map.data.addListener("click", (e) => {
+        let feature = e.feature;
+        if(feature.getProperty("focus") !== true){
+            feature.setProperty("focus", true);
+        } else {
+            feature.setProperty("focus", false);
+        }
+    });
+
+    map.data.addListener("mouseover", (e)=> {
+        let feature = e.feature;
+        let regionName = feature.getProperty("area1");
+        tooltip.css({
+            display: "block",
+            left: e.offset.x,
+            top: e.offset.y,
+        }).text(regionName);
+        map.data.overrideStyle(feature, {
+            fillOpacity: 0.6,
+            strokeWeight: 4, 
+            strokeOpacity: 1,
+        });
+    });
+
+    map.data.addListener("mouseout", (e) => {
+        tooltip.hide().empty();
+        map.data.revertStyle();
+    })
+}
