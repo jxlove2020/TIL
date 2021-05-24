cc2233b082a6fbcba412733690b41535291e35e7
Joseph <jxlove2020@gmail.com>
2021-05-24 10:19:40 +0900

    카카오 api - 중심좌표 가져오기

--- 2021/nodeJs/mapProject/firstmap/public/javascripts/upload.js
+++ 2021/nodeJs/mapProject/firstmap/public/javascripts/upload.js
@@ -6,6 +6,26 @@ const mapOption = {
 
 let map = new daum.maps.Map(mapContainer, mapOption);
 
+// 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+daum.maps.event.addListener(map, 'center_changed', function() {
+
+    // 지도의  레벨을 얻어옵니다
+    var level = map.getLevel();
+
+    // 지도의 중심좌표를 얻어옵니다 
+    var latlng = map.getCenter(); 
+
+    var message = '<p>지도 레벨은 ' + level + ' 이고</p>';
+    message += '<p>중심 좌표는 위도 ' + latlng.getLat() + ', 경도 ' + latlng.getLng() + '입니다</p>';
+
+    // var resultDiv = document.getElementById('result');
+    // resultDiv.innerHTML = message;
+
+    console.log(message);
+
+});
+
+
 let infowindow = new daum.maps.InfoWindow({
     zIndex: 1,
 });
